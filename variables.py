07e89eb09be09a064168d154451708bbb2cb35fc
EgorBelorusov <belorusov2017@mail.ru>
2026-07-12 13:40:04 +0700

    Второй коммит: создание файла variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Переменные каждого типа
+name = "Mark"
+age = 30
+height = 180.4
+is_alive = True
+
+# Вывод на экран
+print(name)
+print(age)
+print(height)
+print(is_alive)
+
+# Вывод типа переменной
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_alive))
+
+
+print(0.1 + 0.2)
