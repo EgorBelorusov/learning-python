bb2aecb8385b2a633d579945a079f185c2cfd9ce
EgorBelorusov <belorusov2017@mail.ru>
2026-07-12 14:54:01 +0700

    4 коммит: ввод с помощью input

--- variables.py
+++ variables.py
@@ -1,28 +1,28 @@
 # Задание 1
 # Переменные каждого типа
-name = "Mark"
-age = 30
-height = 180.4
-is_alive = True
+# name = "Mark"
+# age = 30
+# height = 180.4
+# is_alive = True
 
 # Вывод на экран
-print(name)
-print(age)
-print(height)
-print(is_alive)
-
-# Вывод типа переменной
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_alive))
+# print(name)
+# print(age)
+# print(height)
+# print(is_alive)
+#
+# # Вывод типа переменной
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_alive))
 
 
 print(0.1 + 0.2)
 
 
 # Задание 2
-print(f"Меня зовут {name}. Мне {age} лет. Мой рост {height} см.")
+# print(f"Меня зовут {name}. Мне {age} лет. Мой рост {height} см.")
 
 print(10 + 5)
 print(10 - 5)
@@ -35,3 +35,11 @@ print(10 % 3)
 print(10 % 1)
 
 print("ad" * 3)
+
+
+# Задание 3
+name = input("Введите имя: ")
+age = int(input("Введите возраст: "))
+height = float(input("Введите рост (см): "))
+
+print(f"Привет, меня зовут {name}. Мне {age} лет. Рост {height} см. Через 10 лет тебе будет {age + 10}")
