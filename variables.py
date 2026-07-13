2172858f3ff93d52d371ae7da6e4db02c3a0b241
EgorBelorusov <belorusov2017@mail.ru>
2026-07-13 20:05:30 +0700

    5 коммит: циклы

--- variables.py
+++ variables.py
@@ -21,7 +21,7 @@
 print(0.1 + 0.2)
 
 
-# Задание 2
+# Задание 2 "операции"
 # print(f"Меня зовут {name}. Мне {age} лет. Мой рост {height} см.")
 
 print(10 + 5)
@@ -37,27 +37,49 @@ print(10 % 1)
 print("ad" * 3)
 
 
-# Задание 3
+# Задание 3 "f-строки, input"
 # name = input("Введите имя: ")
 # age = int(input("Введите возраст: "))
 # height = float(input("Введите рост (см): "))
 #
 # print(f"Привет, меня зовут {name}. Мне {age} лет. Рост {height} см. Через 10 лет мне будет {age + 10}")
 
-# Задание 4
-user_age = int(input("Введите ваш возраст: "))
+# Задание 4 "if, elif, else"
+# user_age = int(input("Введите ваш возраст: "))
+#
+# if user_age > 0 and user_age < 150:
+#
+#     print("Возраcт введен корректно")
+#
+#     if user_age < 18:
+#         print("Ты несовершеннолетний")
+#     elif 18 <= user_age <= 65:
+#         print("Ты взрослый")
+#     else:
+#         print("Ты пенсионер")
+#
+# else:
+#     print("Проверьте введенные данные")
 
-if user_age > 0 and user_age < 150:
 
-    print("Возраcт введен корректно")
+# Задание 5 "for, while"
+for i in range(1, 21):
+    if i % 2 == 0:
+        print(i)
 
-    if user_age < 18:
-        print("Ты несовершеннолетний")
-    elif 18 <= user_age <= 65:
-        print("Ты взрослый")
+# 1 вариант решения
+number = ""
+while number != 0:
+    number = int(input("Введите число: "))
+    if number == 0:
+        break
     else:
-        print("Ты пенсионер")
+        print(number)
 
-else:
-    print("Проверьте введенные данные")
-
+# 2 вариант решения
+while True:
+    number = int(input("Введите число: "))
+    if number == 0:
+        break
+    else:
+        print(number)
