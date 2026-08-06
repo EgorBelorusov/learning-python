4e6ed0a13fb12bd2977fab7cec2f1ea14b1cae24
EgorBelorusov <belorusov2017@mail.ru>
2026-08-06 18:30:56 +0700

    Проект Генератор случайного пароля

--- Practice.py
+++ Practice.py
@@ -1,55 +1,68 @@
 # Мини-проект "Угадай число"
 from random import randint
 
-number_randint = randint(1, 100)
-try_flag = 0
-print("Загадал число от 1 до 100. Попробуй отгадать!")
-
-while True:
-    try_flag += 1
-    try:
-        user_input = int(input("Угадайте число: "))
-
-        if user_input in range(1, 101):
-
-            if user_input == number_randint:
-                print("Поздравляю, Вы угадали число!")
-                print(f"Количество попыток: {try_flag}")
-                break
-
-            elif user_input > number_randint:
-                print("Меньше")
-
-
-            else:
-                print("Больше")
-
-        else:
-            print("Вы ввели число за пределами диапазона от 1 до 100. Попробуйте снова.")
-
-    except ValueError:
-        print("Ошибка! Необходимо ввести число!")
-
-
-
-
-
-
-
-
-
+# def guess_number_game():
+#     number_randint = randint(1, 100)
+#     attempts_count = 0
+#     print("Загадал число от 1 до 100. Попробуй отгадать!")
+#
+#     while True:
+#         attempts_count += 1
+#         try:
+#             user_input = int(input("Угадайте число: "))
+#
+#             if user_input in range(1, 101):
+#
+#                 if user_input == number_randint:
+#                     print("Поздравляю, Вы угадали число!")
+#                     print(f"Количество попыток: {attempts_count}")
+#                     return attempts_count
+#
+#                 elif user_input > number_randint:
+#                     print("Меньше")
+#
+#
+#                 else:
+#                     print("Больше")
+#
+#             else:
+#                 print("Вы ввели число за пределами диапазона от 1 до 100. Попробуйте снова.")
+#
+#         except ValueError:
+#             print("Ошибка! Необходимо ввести число!")
+#
+#
+#
+#
+#
+# print(guess_number_game())
 
 
 
 
+# ================================================================
+# Мини-проект "Генератор пароля"
+import string
+from random import choice
+
+def generate_password(length):
+    set_chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
+    password_storage = ""
+
+    for i in range(length):
+        char = choice(set_chars)
+        password_storage += char
+
+    return password_storage
 
 
 
+try:
+    user_input = int(input("Введите длину пароля: "))
+    print(generate_password(user_input))
 
-
-
-
-
+except ValueError:
+    print("Ошибка! Введите число.")
 
 
 
