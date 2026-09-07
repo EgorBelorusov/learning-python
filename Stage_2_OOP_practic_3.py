ef7553a5243f77a1a8628e184a71c390d2e1a376
EgorBelorusov <belorusov2017@mail.ru>
2026-09-07 19:53:50 +0700

    Практика ООП этап 2 'Задача 4'

--- Stage_2_OOP_practic_3.py
+++ Stage_2_OOP_practic_3.py
@@ -1,4 +1,6 @@
 # Задача 1 — Класс с боевой механикой
+from os import name
+
 from requests.packages import target
 
 
@@ -48,6 +50,7 @@ class Character:
         self.name = name
         self.hp = 100
         self.attack_power = attack_power
+        self.inventory = Inventory()
 
 
     def attack(self, target):
@@ -60,6 +63,14 @@ class Character:
         return self.hp > 0
 
 
+    def use_item(self, item_name: str):
+        for item in self.inventory.items:
+            if item.name == item_name:
+                self.attack_power += item.attack_bonus  # Исправлено: attack_bonus вместо bonus
+                self.inventory.remove_item(item_name)
+                print(f"{self.name} использовал {item_name}. Сила атаки увеличена на {item.attack_bonus}!")
+                return
+        print(f"Предмет {item_name} не найден в инвентаре")
 
 
 class Warrior(Character):
@@ -79,12 +90,77 @@ class Mage(Character):
 
 
 
+
+
+
+# Задача 3 Инвентарь
+class Item:
+    def __init__(self, name: str, value: int, attack_bonus: int):
+        self.name = name
+        self.value = value
+        self.attack_bonus = attack_bonus
+
+
+    def __str__(self):
+        return f"''{self.name}'', цена: {self.value}, бонусный урон: {self.attack_bonus}"
+
+
+
+
+
+class Inventory:
+    def __init__(self):
+        self.items = []
+
+
+    def add_item(self, name: str, value: int, bonus: int):
+        item_inventory = Item(name, value, bonus)
+        self.items.append(item_inventory)
+
+        return f"Предмет {item_inventory.name} добавлен в инвентарь"
+
+
+    def remove_item(self, name):
+        for item in self.items:
+            if item.name == name:
+                self.items.remove(item)
+                return f"Предмет '{item.name}' был удалён из инвентаря"
+
+        return f"Предмет не найден"
+
+
+
+    def total_value(self):
+        total = 0
+        for item in self.items:
+            total += item.value
+
+        return f"Общая сумма всех предметов: {total}"
+
+
+    def show_items(self):
+        for num, item in enumerate(self.items):
+            print(f"{num + 1}. {item}")
+
+        return "Список выведен"
+
+
+
+
+
+
+
+
+
 warrior = Warrior(name="Воин", attack_power=40)
 mage = Mage(name="Маг", attack_power=60)
 monster = Character(name="Монстр", attack_power=30)
 monster.hp = 300
 attackers = [warrior, mage]
-
+print(warrior.inventory.show_items())
+warrior.inventory.add_item("Меч", 50, 15)
+warrior.use_item("Меч")
+print(warrior.inventory.show_items())
 
 while monster.is_alive() and any(a.is_alive() for a in attackers):
 
@@ -100,6 +176,25 @@ while monster.is_alive() and any(a.is_alive() for a in attackers):
 
 winner = f"{monster.name} победил" if any(not a.is_alive() for a in attackers) else f"{warrior.name} и {mage.name} победили"
 print(f"Победитель: {winner}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+warrior = Warrior(name="Воин", attack_power=40)
+
+print(warrior.inventory.total_value())
+print(warrior.inventory.show_items())
+
 
 
 
